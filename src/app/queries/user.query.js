2d709b920beb668788678f29e34411db8026b7e3
mekkymayata <okpallannameka@gmail.com>
2020-11-22 19:28:39 +0100

    update user query

--- src/app/queries/user.query.js
+++ src/app/queries/user.query.js
@@ -1,20 +1,21 @@
 const query = {
+    createUser: `
+        INSERT INTO users (
+            id, name, username, password, email,telephone, gender, website, bio,category
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        RETURNING *
+    `,
     findUserById: `
-        SELECT
-            *
-        FROM
-            users
-        WHERE
-            id = $1
+        SELECT * FROM users WHERE id = $1
     `,
     findUserByEmail: `
-        SELECT
-            *
-        FROM
-            users
-        WHERE
-            email = $1
-    `
+        SELECT * FROM users WHERE email = $1
+    `,
+    findUserByToken: `
+        SELECT * FROM users WHERE password_reset_token = $1
+    `,
+    
+
 };
 
 export default query;

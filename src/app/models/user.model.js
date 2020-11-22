366eb06372daa6561273b8fc299e242eebbaf6e7
mekkymayata <okpallannameka@gmail.com>
2020-11-22 19:28:09 +0100

    create user subroutine

--- src/app/models/user.model.js
+++ src/app/models/user.model.js
@@ -5,6 +5,37 @@ import logger from '../../config/logger';
 
 class User {
     /**
+     * @description - Create a new user on the database
+     * @param { Object } userData - the user records
+     * @returns { Object } created user records or error
+     */
+    static async createUser(userData) {
+        try {
+            const {
+                id, 
+                name: name, 
+                username: username,
+                password: password, 
+                email: email,
+                telephone: telephone, 
+                gender: gender, 
+                website: website, 
+                bio: bio,
+                category: category
+            } = userData;
+            const user = await db.oneOrNone(
+                userQuery.createUser,
+                [id, name, username, password, email, telephone, gender, website, bio, category]
+                );
+            return user;
+        } catch (err) {
+            logger.error(`[${moment.format('DD-MM-YYYY, h:mm:ss')}]`,
+            'Error: Failed to create user from user.model', err);
+            throw new Error('Failed to create user');
+        }
+    }
+
+    /**
      * @description - Fetch single user from database
      * @param { Number } id - the user id
      * @returns { Object } user object

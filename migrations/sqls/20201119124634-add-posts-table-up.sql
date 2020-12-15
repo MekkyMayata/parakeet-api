09473fc347ea772a29e0fa832748736dfe656f05
mekkymayata <okpallannameka@gmail.com>
2020-12-15 22:26:07 +0100

    update post table

--- migrations/sqls/20201119124634-add-posts-table-up.sql
+++ migrations/sqls/20201119124634-add-posts-table-up.sql
@@ -1,10 +1,11 @@
 /* Replace with your SQL commands */
 CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
+    post_id VARCHAR(100) NOT NULL,
     user_id VARCHAR(100) NOT NULL REFERENCES users (id),
     post_path TEXT NOT NULL,
     post_caption TEXT,
     post_latitude NUMERIC,
     post_longitude NUMERIC,
-    post_date TIMESTAMPTZ DEFAULT NOW()
+    post_date TIMESTAMPTZ DEFAULT NOW(),
+    PRIMARY KEY (post_id)
 );

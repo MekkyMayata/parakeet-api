c54753e0c0fb46c0e9c41cd08120b4fa8019ffa1
mekkymayata <okpallannameka@gmail.com>
2020-12-16 14:36:32 +0100

    update post query

--- src/app/queries/post.query.js
+++ src/app/queries/post.query.js
@@ -1,24 +1,29 @@
 const query = {
-  createImagePost: `
+  createNewPost: `
     INSERT INTO posts (
-      id, user_id, post_path, post_caption, post_latitude, post_longitude
-    ) VALUES ($1, $2, $3, $4, $5, $5, $6)
+      post_id, user_id, post_path, post_caption, post_latitude, post_longitude
+    ) VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING *
   `,
-  deleteImagePostByPost_path: `
-      DELETE FROM users WHERE post_path = $1
+  deleteImagePostById: `
+      DELETE FROM posts 
+      WHERE post_id = $1 AND user_id = $2
   `,
   findImagePostById:  `
-      SELECT * FROM posts WHERE id = $1
+      SELECT * FROM posts WHERE post_id = $1
+  `,
+  verifyImagePostById: `
+      SELECT * FROM posts WHERE post_id = $1
   `,
   findImagePostByPath: `
       SELECT * FROM posts WHERE post_path = $1
   `,
-  getUserImagePost: `
+  fetchPosts: `
       SELECT
+        users.id,
         users.username,
         users.category,
-        posts.id AS post_id, 
+        posts.post_id, 
         posts.post_path, 
         posts.post_caption, 
         posts.post_latitude, 
@@ -26,8 +31,15 @@ const query = {
         posts.post_date
       FROM posts
       INNER JOIN users ON users.id = posts.user_id
-      ORDER BY post_date DESC
-
+      WHERE posts.user_id = $1
+      ORDER BY posts.post_date DESC
+      OFFSET $2
+      LIMIT $3
+  `,
+  getPostsCount: `
+      SELECT count(*) AS count 
+      FROM posts
+      INNER JOIN users ON users.id = posts.user_id
   `
 }
 

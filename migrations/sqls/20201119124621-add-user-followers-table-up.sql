a9f48a22dcc346bf64b506839e82b6dcc428acf3
mekkymayata <okpallannameka@gmail.com>
2020-12-19 19:13:14 +0100

    alter user_followers table and add queries

--- migrations/sqls/20201119124621-add-user-followers-table-up.sql
+++ migrations/sqls/20201119124621-add-user-followers-table-up.sql
@@ -1,9 +1,9 @@
 /* Replace with your SQL commands */
 CREATE TABLE IF NOT EXISTS user_followers (
     user_follower_id SERIAL,
-    userId VARCHAR(100),
+    user_id VARCHAR(100),
     follower_id VARCHAR(100),
     PRIMARY KEY (user_follower_id),
-    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (follower_id) REFERENCES users(id)
 );

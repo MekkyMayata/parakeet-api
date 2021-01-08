8183bad02843d8ac1b2e8bd8bfc4b5722bce0df1
mekkymayata <okpallannameka@gmail.com>
2021-01-08 18:54:40 +0100

    edit github actions

--- scripts/testKey.sh
+++ scripts/testKey.sh
@@ -4,6 +4,8 @@
 # public and private keys for test
 # environment
 
+cd ../src/config/environment/
+pwd
 openssl genrsa -out test_privates.key 1024
 openssl rsa -in test_private.key -out test_publics.key -pubout -outform PEM
 exit 0

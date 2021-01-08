957be14226862d6d5c0bd321e36091a927fae2ec
mekkymayata <okpallannameka@gmail.com>
2021-01-08 12:08:30 +0100

    possible fix test environment key issue

---
+++ scripts/testKey.sh
@@ -0,0 +1,10 @@
+#! /bin/bash --
+
+# A script to create the necessary 
+# public and private keys for test
+# environment
+
+cd ../src/config/environment/
+openssl genrsa -out test_privates.key 1024
+openssl rsa -in test_private.key -out test_publics.key -pubout -outform PEM
+exit 0

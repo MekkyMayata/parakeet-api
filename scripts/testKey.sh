52b47156551c14a20407d2064b08303d038d90a8
mekkymayata <okpallannameka@gmail.com>
2021-01-08 18:16:08 +0100

    set working-dir for  key gen

--- scripts/testKey.sh
+++ scripts/testKey.sh
@@ -4,7 +4,6 @@
 # public and private keys for test
 # environment
 
-cd ../src/config/environment/
 openssl genrsa -out test_privates.key 1024
 openssl rsa -in test_private.key -out test_publics.key -pubout -outform PEM
 exit 0
